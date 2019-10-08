f23795b69595ad4f804106314a34d8846b14fbab
knepjow <mortonftc@gmail.com>
2019-10-07 19:05:16 -0500

    Creation on initial Hardware class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMecanum.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class HardwareMecanum
+{
+    //Base
+    public DcMotor leftFront;
+    public DcMotor leftBack;
+    public DcMotor rightBack;
+    public DcMotor rightFront;
+    public BNO055IMU imu;
+
+    //Collector
+    public CRServo leftCollector;
+    public CRServo rightCollector;
+
+    //Mast
+    public DcMotor mastVertical;
+    public DcMotor mastRotator;
+
+    //Arm
+    public CRServo armExtender;
+    public Servo armRotater;
+    public Servo gripper;
+
+    public HardwareMap hwMap;
+
+    public void init(HardwareMap hwMap)
+    {
+        //BASE
+        this.hwMap = hwMap;
+        leftFront = hwMap.get(DcMotor.class, "leftFront");
+        leftBack = hwMap.get(DcMotor.class, "leftBack");
+        rightBack = hwMap.get(DcMotor.class, "rightBack");
+        rightFront = hwMap.get(DcMotor.class, "rightFront");
+
+        //COLLECTOR
+        leftCollector = hwMap.get(CRServo.class, "leftCollector");
+        rightCollector = hwMap.get(CRServo.class, "rightCollector");
+
+        //MAST
+        mastVertical = hwMap.get(DcMotor.class, "mastVertical");
+        mastRotator = hwMap.get(DcMotor.class, "mastRotator");
+
+        //ARM
+        armExtender = hwMap.get(CRServo.class, "armExtender");
+        armRotater = hwMap.get(Servo.class, "armRotater");
+        gripper = hwMap.get(Servo.class, "gripper");
+
+    }
+}
