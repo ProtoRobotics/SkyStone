77adcacac67572bedb5c40e805b7f11981a9b544
Michael Kumar <michaelrk73@hotmail.com>
2019-10-07 20:27:12 -0500

    we finally did it yayyy!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Collector.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+        import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class Collector
+{
+    OpMode teleOpClass; //Used for telemetry.
+    HardwareMecanum robot; //Used to move robot parts.
+
+    Gamepad gamepad1; //Driver
+    Gamepad gamepad2; //Gunner
+
+    public Collector(OpMode teleOpClass, HardwareMecanum robot, Gamepad gamepad1, Gamepad gamepad2)
+    {
+        this.gamepad1 = gamepad1;
+        this.gamepad2 = gamepad2;
+        this.robot = robot;
+        this.teleOpClass = teleOpClass;
+        init();
+    }
+
+    //
+    public void init()
+    {
+
+    }
+
+    public void doLoop()
+    {
+        if (gamepad1.a)
+        {
+            teleOpClass.telemetry.addLine("Hello, you are pressing the A button.");
+            teleOpClass.telemetry.update();
+
+
+        }
+    }
+}
