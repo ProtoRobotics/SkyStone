4d29db0bfb9fbd3c653b9fb155a9f2bbb9bb334a
knepjow <notmyemail@gmail.com>
2019-10-18 11:54:10 -0500

    Added methods and created basic drive functionality.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 public class Base
@@ -10,6 +11,8 @@ public class Base
 
     private Gamepad gamepad1; //Driver
     private Gamepad gamepad2; //Gunner
+
+    public static final int COUNTS_PER_INCH = 10; //TODO
 
     public Base(OpMode teleOpClass, HardwareMecanum robot, Gamepad gamepad1, Gamepad gamepad2)
     {
@@ -27,10 +30,67 @@ public class Base
 
     public void doLoop()
     {
-        if (gamepad1.a)
-        {
-            teleOpClass.telemetry.addLine("Hello, you are pressing the A button.");
-            teleOpClass.telemetry.update();
+        double lFrontDrive = 0;
+        double rFrontDrive = 0;
+        double lRearDrive = 0;
+        double rRearDrive = 0;
+
+        if (Math.abs(gamepad1.left_stick_y) > 0 || Math.abs(gamepad1.right_stick_y) > 0)
+        { // forward/reverse
+            lFrontDrive = -gamepad1.left_stick_y;
+            lRearDrive = -gamepad1.left_stick_y;
+            rFrontDrive = gamepad1.right_stick_y;
+            rRearDrive = gamepad1.right_stick_y;
         }
+        else if (Math.abs(gamepad1.left_stick_x) > 0)
+        { // left/right shift
+            lFrontDrive = -gamepad1.left_stick_y;
+            lRearDrive = gamepad1.left_stick_y;
+            rFrontDrive = -gamepad1.left_stick_y;
+            rRearDrive = gamepad1.left_stick_y;
+        }
+
+        robot.leftBack.setPower(lFrontDrive);
+        robot.rightFront.setPower(rFrontDrive);
+        robot.leftBack.setPower(lRearDrive);
+        robot.rightBack.setPower(rRearDrive);
+    }
+
+    public void encoderDriveInches(double leftInches, double rightInches, double power)
+    {
+        encoderDriveCounts((int) leftInches * COUNTS_PER_INCH, (int) rightInches * COUNTS_PER_INCH, power);
+    }
+
+    public void encoderDriveCounts(int leftCounts, int rightCounts, double power)
+    {
+        robot.leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        robot.leftBack.setTargetPosition(leftCounts);
+        robot.leftFront.setTargetPosition(leftCounts);
+        robot.rightBack.setTargetPosition(rightCounts);
+        robot.rightFront.setTargetPosition(rightCounts);
+
+        robot.leftBack.setPower(power);
+        robot.leftFront.setPower(power);
+        robot.rightBack.setPower(power);
+        robot.rightFront.setPower(power);
+    }
+
+    public void rotateIMU(double degrees) //Positive = clockwise, negative = counter-clockwise
+    {
+
+    }
+
+    public void rotateDegreesEncoder(double degrees)
+    {
+
     }
 }
