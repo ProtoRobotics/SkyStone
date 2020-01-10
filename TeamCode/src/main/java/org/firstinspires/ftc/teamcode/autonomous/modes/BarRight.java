2607bde68bfc078ecf55ab49f85314332528ebde
Kevkaik <54111130+Kevkaik@users.noreply.github.com>
2020-01-10 16:59:53 -0600

    Merge branch 'master' of https://github.com/ProtoRobotics/SkyStone
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousBar.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/modes/BarLeft.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/modes/BarRight.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/modes/BarRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/modes/BarRight.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.Base;
+import org.firstinspires.ftc.teamcode.HardwareMecanum;
 import org.firstinspires.ftc.teamcode.autonomous.AutonomousBar;
 import org.firstinspires.ftc.teamcode.autonomous.AutonomousPosition;
 
@@ -14,5 +16,23 @@ public class BarRight extends LinearOpMode
     public void runOpMode() throws InterruptedException
     {
         AutonomousBar autoBar = new AutonomousBar(this, AutonomousPosition.RIGHT);
+        HardwareMecanum robot = new HardwareMecanum();
+        robot.init(hardwareMap);
+        Base base = new Base(this, robot, gamepad1, gamepad2);
+        base.encoderDriveInches(22,22,.5,true);
+        Thread.sleep(900);
+        base.encoderCrabsteer(0,22,.5,true);
+        Thread.sleep(900);
+        base.encoderDriveInches(5,5,.2,true);
+        Thread.sleep(900);
+        base.encoderDriveInches(-5,-5,.2,true);
+        Thread.sleep(900);
+        base.encoderCrabsteer(1,80,.5,true);
+        Thread.sleep(900);
+        base.encoderDriveInches(6,6,.1,true);
+        Thread.sleep(900);
+        base.encoderDriveInches(-5,-5,.1,true);
+        Thread.sleep(900);
+        base.encoderCrabsteer(0,45,.5,true);
     }
 }
