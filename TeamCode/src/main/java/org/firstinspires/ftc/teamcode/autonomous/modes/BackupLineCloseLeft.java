135e5d5ff9c7357773484c8735f725aced1cc09f
ndelgado328 <ndelgado328@icloud.com>
2020-01-31 16:35:09 -0600

    Auto box fine tuning

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/modes/BackupLineCloseLeft.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.autonomous.modes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.autonomous.AutonomousBox;
+import org.firstinspires.ftc.teamcode.autonomous.AutonomousPosition;
+import org.firstinspires.ftc.teamcode.autonomous.BackUpLineWall;
+
+@Autonomous(name="BackupLineCloseLeft")
+public class BackupLineCloseLeft extends LinearOpMode
+{
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        BackUpLineWall Backup = new BackUpLineWall(this, AutonomousPosition.LEFT);
+    }
+}
