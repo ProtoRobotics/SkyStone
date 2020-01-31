f98c1b5cd27c4b7f20af0aa9c2478ea55fe1cab4
Kevkaik <54111130+Kevkaik@users.noreply.github.com>
2020-01-31 16:45:49 -0600

    Merge branch 'master' of https://github.com/ProtoRobotics/SkyStone
    Hi
    
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousBar.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/modes/BarLeft.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/modes/BarRight.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BackupLineCenter.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.Arm;
+import org.firstinspires.ftc.teamcode.Base;
+import org.firstinspires.ftc.teamcode.Collector;
+import org.firstinspires.ftc.teamcode.HardwareMecanum;
+import org.firstinspires.ftc.teamcode.Mast;
+
+public class BackupLineCenter
+{
+    Base base;
+    Collector collector;
+    Mast mast;
+    Arm arm;
+
+    HardwareMecanum robot;
+
+    LinearOpMode autonomousClass;
+    AutonomousPosition autonomousPosition;
+
+    public BackupLineCenter (LinearOpMode autonomousClass, AutonomousPosition autonomousPosition) throws InterruptedException //direction: 0 = left, 1 = right.
+    {
+        this.autonomousClass = autonomousClass;
+        this.autonomousPosition = autonomousPosition;
+
+        robot = new HardwareMecanum();
+        robot.init(autonomousClass.hardwareMap);
+
+        base = new Base(autonomousClass, robot, autonomousClass.gamepad1, autonomousClass.gamepad2);
+        collector = new Collector(autonomousClass, robot, autonomousClass.gamepad1, autonomousClass.gamepad2);
+        mast = new Mast(autonomousClass, robot, autonomousClass.gamepad1, autonomousClass.gamepad2);
+        arm = new Arm(autonomousClass, robot, autonomousClass.gamepad1, autonomousClass.gamepad2);
+
+        mast.resetMastEncoders();
+
+        autonomousClass.waitForStart();
+
+        runOpMode();
+    }
+
+    public void runOpMode() throws InterruptedException
+    {
+        {
+            //all directional comments based on start position of AutonomousPosition.RIGHT
+            //directions would be reversed for start on LEFT side
+
+            int rotationDegrees;
+            base.hookUp();
+            mast.moveCounts(1100,.3);
+            arm.moveSeconds(5.5,-1);
+            robot.gripperRotator.setPosition(arm.GRIPPER_ROTATOR_POS_2);
+            robot.leftGripper.setPosition(arm.GRIPPER_LEFT_OPEN);
+            robot.rightGripper.setPosition(arm.GRIPPER_RIGHT_OPEN);
+
+            //move forward away from wall
+            base.encoderDriveInches(24,24,.3,true);
+            Thread.sleep(500);
+
+            //crabsteer to the left to align with middle stone in rightmost set of 3
+            int crabDirection = (autonomousPosition == AutonomousPosition.RIGHT) ? 0 : 1;
+            base.encoderCrabsteer(crabDirection,12.5,.5,true);
+            Thread.sleep(2000);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/modes/BackupLineCenterRight.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.autonomous.modes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.autonomous.AutonomousBox;
+import org.firstinspires.ftc.teamcode.autonomous.AutonomousPosition;
+import org.firstinspires.ftc.teamcode.autonomous.BackupLineCenter;
+
+@Autonomous(name="BackupLineCenter")
+public class BackupLineCenterRight extends LinearOpMode
+{
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        BackupLineCenter autoBox = new BackupLineCenter(this, AutonomousPosition.RIGHT);
+    }
+}
